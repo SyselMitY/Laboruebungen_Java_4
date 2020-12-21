d5d2ffcf2d9dd4738e9f79071acbc2dab213906e
Peter Sysel <peter@sysel.at>
2020-12-21 10:57:10 +0100

    labor 6 repository finished

--- Labor_06/src/main/java/schuelerdb/JdbcSchuelerRepository.java
+++ Labor_06/src/main/java/schuelerdb/JdbcSchuelerRepository.java
@@ -1,7 +1,8 @@
 package schuelerdb;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,26 +16,107 @@ public class JdbcSchuelerRepository implements SchuelerRepository {
 
     @Override
     public int persistSchueler(List<Schueler> lst) throws SQLException {
+        deleteIfExists();
+        int updateCount = 0;
+        for (Schueler schueler : lst) {
+            updateCount += addSchueler(schueler);
+        }
         return 0;
+    }
+
+    private void createSchuelerTable() throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE schueler(" +
+                    "int katalognummer," +
+                    "varchar(10) klasse," +
+                    "varchar(64) vorname," +
+                    "varchar(64) familienname," +
+                    "varchar(1) geschlecht)");
+        }
+    }
+
+    private void deleteIfExists() throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS schueler");
+        }
+    }
+
+    public int addSchueler(Schueler schueler) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO schueler VALUES(?,?,?,?,?)")) {
+            statement.setInt(1, schueler.getKatalognummer());
+            statement.setString(2, schueler.getKlasse());
+            statement.setString(3, schueler.getVorname());
+            statement.setString(4, schueler.getFamilienname());
+            statement.setString(5, String.valueOf(schueler.getGeschlecht()));
+            statement.execute();
+            return statement.getUpdateCount();
+        }
     }
 
     @Override
     public int deleteAll() throws SQLException {
-        return 0;
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("DELETE FROM schueler");
+            return statement.getUpdateCount();
+        }
     }
 
     @Override
     public List<Schueler> findSchuelerByKlasse(String klasse) throws SQLException {
-        return null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT katalognummer,klasse,vorname,nachname,geschlecht" +
+                        "FROM schueler" +
+                        "WHERE klasse=?")) {
+            statement.setString(1, klasse);
+            statement.execute();
+            return getSchuelerFromResultSet(statement.getResultSet());
+        }
+    }
+
+    private List<Schueler> getSchuelerFromResultSet(ResultSet resultSet) throws SQLException {
+        List<Schueler> schuelers = new ArrayList<>();
+        while (resultSet.next()) {
+            int katalognummer = resultSet.getInt("katalognummer");
+            String klasse = resultSet.getString("klasse");
+            String vorname = resultSet.getString("vorname");
+            String familienname = resultSet.getString("familienname");
+            String geschlecht = resultSet.getString("geschlecht");
+            schuelers.add(new Schueler(familienname, vorname, geschlecht.charAt(0), katalognummer, klasse));
+        }
+        return schuelers;
     }
 
     @Override
     public List<Schueler> findSchuelerByGeschlecht(char geschlecht) throws SQLException {
-        return null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT katalognummer,klasse,vorname,nachname,geschlecht" +
+                        "FROM schueler" +
+                        "WHERE geschlecht=?")) {
+            statement.setString(1, String.valueOf(geschlecht));
+            statement.execute();
+            return getSchuelerFromResultSet(statement.getResultSet());
+        }
     }
 
     @Override
     public Map<String, Integer> getKlassen() throws SQLException {
-        return null;
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(
+                    "SELECT klasse, COUNT(katalognummer)\n" +
+                            "FROM schueler\n" +
+                            "GROUP BY klasse\n" +
+                            "ORDER BY klasse");
+            return getKlassenMapFromResultSet(statement.getResultSet());
+        }
+    }
+
+    private Map<String, Integer> getKlassenMapFromResultSet(ResultSet resultSet) throws SQLException {
+        Map<String, Integer> klassenMap = new HashMap<>();
+        while (resultSet.next()) {
+            String klasse = resultSet.getString("klasse");
+            int schueler = resultSet.getInt("COUNT(katalognummer)");
+            klassenMap.put(klasse, schueler);
+        }
+        return klassenMap;
     }
 }
