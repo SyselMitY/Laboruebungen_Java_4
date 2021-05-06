255138da69c24f6ac4c4b3622617784dde4d625a
Peter Sysel <peter@sysel.at>
2021-05-06 16:33:08 +0200

    Spring Theory example init

---
+++ Theorie_Bsp1/src/test/java/cf/soisi/spring_employee/SpringEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.soisi.spring_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
